af3564bae845e83f552deaad5dd04035505eddd8
chinmaydas96 <chinmaydasbat@gmail.com>
2017-04-23 03:07:30 +0530

    maximize loot

--- Algorithmic Toolbox/week-3/assignment/2. fractional_knapsack.py
+++ Algorithmic Toolbox/week-3/assignment/2. fractional_knapsack.py
@@ -1,32 +1,42 @@
 # Uses python3
-import sys
 
+def get_optimal_value(items, capacity, weights, values):
 
-def get_optimal_value(capacity, weights, values):
-    value = 0.
-    # write your code here
+	value = 0
+	A = [0] * items
 
-    return value
+	for i in range(items):
+		if capacity == 0:
+			return value
+		else:
+			b = max_index(values, weights, items)
+			a = min(weights[b], capacity)
+			value = value +  a * (values[b] / weights[b])
+			weights[b] -= a
+			values[b] -= value
+			A[i] = A[i] + a
+			capacity -= a
+	return value
+	
+
+def max_index(values,weights,items):
+	index = -1
+	maximum = 0
+	for i in range(items):
+		if ( weights[i] > 0 and maximum < values[i] / weights[i]):
+			maximum  = values[i] / weights[i]
+			index = i
+	return index
 
 
 if __name__ == "__main__":
-	values = []
-	weights = []
-	n = input().split()
-	items = int(n[0])
-	capacity = int(n[1])
-	for i in range(1, items + 1):
-	    m = input().split()
-	    values.append(int(m[0]))
-	    weights.append(int(m[1]))
-
-	print(items)
-	print(capacity)
-	print(values)
-	print(weights)
-
-
-
-    # print("{:.10f}".format(opt_vvalues=data[2:(2 * n + 2):2]
-    #                        weights=data[3:(2 * n + 2):2]
-    # opt_value=get_optimal_value(capacity, weights, values)alue))
+    values = []
+    weights = []
+    n = input().split()
+    items = int(n[0])
+    capacity = int(n[1])
+    for i in range(1, items + 1):
+        m = input().split()
+        values.append(int(m[0]))
+        weights.append(int(m[1]))   
+    print ("%.4f" % get_optimal_value(items, capacity, weights, values))
